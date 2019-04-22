06830fad77d6baf64c3dbadfd64ac014bb74e4af
JPantana <joshpantana@me.com>
2019-04-22 18:54:50 -0500

    styled movie cards

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -7,7 +7,15 @@ let movies = [];
 const domStringBuilder = () => {
   let domString = '';
   movies.forEach((movie) => {
-    domString += `<h3>${movie.name}</h3>`;
+    domString += `<div id="${movie.id}" class="shadow movie rounded-lg text-white space-justify-content-around bg-dark card col-sm border text-center">`;
+    domString += `  <h1 class="h1 rounded-lg text-uppercase bg-primary">${movie.name}</h1>`;
+    domString += '  <hr class="bg-white">';
+    domString += `  <h4>${movie.genre}</h4>`;
+    domString += `  <h5>${movie.releaseDate}</h5>`;
+    domString += '  <hr class="bg-white">';
+    domString += `  <p><em class="text-white">Description:</em><br>${movie.description}</p>`;
+    domString += `  <h5 class="text-monospace">${movie.locations.length} locations</h5>`;
+    domString += '</div>';
   });
   util.printToDom('movies', domString);
 };

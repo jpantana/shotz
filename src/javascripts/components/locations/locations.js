e4167c270b142c9356bf4a79081821e69048f92d
JPantana <joshpantana@me.com>
2019-04-22 19:26:28 -0500

    color head function

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -4,11 +4,32 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div class="bg-white text-dark locations text-center card col-2" id="${locations.id}">`;
-    domString += `  <div class="card-header">${location.name}</div>`;
+    domString += `  <div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
     domString += `  <img src="${location.imageUrl}" alt="image of ${locations.id}">`;
     domString += `  <h5 class"text-monospace">${location.address}</h5>`;
     domString += `  <h5 class"text-monospace">${location.shootTime}</h5>`;
